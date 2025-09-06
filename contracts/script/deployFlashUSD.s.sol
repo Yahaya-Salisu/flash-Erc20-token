8f8887fb745077ed5de2bb20e9c55fdf9dfc1a6b
Yahaya Salisu BabsAudits <yahayasalisubabs@gmail.com>
2025-09-06 09:29:30 +0100

    Update deployFlashUSD.s.sol

--- contracts/script/deployFlashUSD.s.sol
+++ contracts/script/deployFlashUSD.s.sol
@@ -1,1 +1,17 @@
-1
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import { Script } from "forge-std/Script.sol";
+import "../src/flashUSD.sol";
+
+contract deployFlashUSD is Script {
+    flashUSD public fUSD;
+
+    function run() public {
+
+        vm.startBroadcast();
+        fUSD = new flashUSD();
+        vm.stopBroadcast();
+    }
+}
