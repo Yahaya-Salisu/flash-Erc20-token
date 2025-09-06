ad3056cb6026c359e2869cf727990715fbc52446
Yahaya Salisu BabsAudits <yahayasalisubabs@gmail.com>
2025-09-06 09:30:54 +0100

    Update flashUSD.sol

--- contracts/src/flashUSD.sol
+++ contracts/src/flashUSD.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.4.0
+// Compatible with OpenZeppelin Contracts ^5.4.0 
 pragma solidity ^0.8.27;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
