42b83afebab824cc247c526edf0f495caac4d1a8
Yahaya Salisu BabsAudits <yahayasalisubabs@gmail.com>
2025-09-06 09:31:19 +0100

    Update flashUSD.t.sol

--- contracts/test/flashUSD.t.sol
+++ contracts/test/flashUSD.t.sol
@@ -1,1 +1,22 @@
-1
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.27;
+
+import { Test } from "forge-std/Test.sol";
+import "../src/flashUSD.sol";
+
+contract flashUSD is flashUSD, Test {
+    flashUSD public fUSD;
+    address public owner;
+    address public newOwner;
+    address public User1;
+    address public User2;
+
+    function setUp() public {
+        fUSD = new flashUSD();
+        owner = address(this);
+        User1 = makeAddr("User1");
+        User2 = makeAddr("User2");
+        newOwner = makeAddr("newOwner");
+    }
+}
